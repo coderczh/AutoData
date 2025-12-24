9460b926eb042044e52e3526d70b8a23163b4f59
天天向上 <imchenzh@foxmail.com>
2025-12-24 21:36:59 +0800

    20251224_01提交

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="">
-    <el-button type="primary">Primary</el-button>
-  </div>
+  <div class="contain"></div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.contain {
+  background-color: #2c73eb;
+  width: 50vw;
+  height: 100vh;
+}
+</style>
